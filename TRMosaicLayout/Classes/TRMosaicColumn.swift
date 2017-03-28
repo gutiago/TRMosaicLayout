cfb7af9c976ecbc9863ef80ecc53ebd5464fcf52
Gustavo Tiago <gu.tiagosilva@gmail.com>
2017-03-28 10:36:36 -0300

    Update TRMosaicColumn.swift

--- TRMosaicLayout/Classes/TRMosaicColumn.swift
+++ TRMosaicLayout/Classes/TRMosaicColumn.swift
@@ -6,12 +6,18 @@
 //
 //
 
+import UIKit
+
 struct TRMosaicColumns {
     
     var columns:[TRMosaicColumn]
     
-    var smallestColumn:TRMosaicColumn {
+    var smallestColumn: TRMosaicColumn {
         return columns.sorted().first!
+    }
+    
+    var biggestColumn: TRMosaicColumn {
+        return columns.sorted().last!
     }
     
     init() {
@@ -28,15 +34,58 @@ struct TRMosaicColumns {
     }
 }
 
-struct TRMosaicColumn {
+struct TRRandomSize {
+    private static var lastMosaic = 0
+    private static var randomMosaic = [TRMosaicCellType]()
     
-    var columnHeight:CGFloat
+    static func getRandomMosaic(at indexPath: IndexPath) -> TRMosaicCellType {
+        return randomMosaic[indexPath.item]
+    }
+    
+    static func generateMosaicArray(for numberOfItems: Int, appendingObjects: Bool) {
+        if !appendingObjects {
+            randomMosaic.removeAll()
+        }
+        for _ in 0..<numberOfItems {
+            guard randomMosaic.count < numberOfItems else {
+                return
+            }
+            randomMosaic.append(contentsOf: randomSize())
+        }
+    }
+    
+    static func random(_ range: UInt32) -> Int {
+        return Int(arc4random_uniform(range))
+    }
+    
+    static func randomSize() -> [TRMosaicCellType] {
+        let randomNumber = random(2)
+        var options = [0, 1, 2]
+        options.remove(at: options.index(of: lastMosaic)!)
+        
+        let switchValue = randomNumber == 0 ? options.first! : options.last!
+        
+        TRRandomSize.lastMosaic = switchValue
+        switch switchValue {
+        case 0:
+            return [.small, .small, .small]
+        case 1:
+            return [.medium, .medium]
+        default:
+            return [.big, .small, .small]
+        }
+    }
+    
+}
+
+struct TRMosaicColumn {
+    var columnHeight: CGFloat
     
     init() {
         columnHeight = 0
     }
     
-    mutating func appendToColumn(withHeight height:CGFloat) {
+    mutating func appendToColumn(withHeight height: CGFloat) {
         columnHeight += height
     }
 }
